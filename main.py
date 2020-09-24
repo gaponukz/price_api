2bbd3a1a143c657df1a71cb20ae4a7a6b3a9ac0f
lil_gappy_69 <49754258+gaponukz@users.noreply.github.com>
2020-09-24 17:28:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,44 @@ from time import sleep
 from fastapi import FastAPI
 # pip install -r libs.txt
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time, random
+
+class TemplateBot(object):
+    def __init__(self, show = False, debug = False) -> None:
+        options = Options()
+        if not show: options.add_argument("--headless")
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--disable-logging') 
+        self.driver = webdriver.Chrome(options = options)
+        self.debug = debug
+
+    def protected_sleep(self, time_to_sleep = None) -> None:
+        if not time_to_sleep:
+            list_of_seconds = [x / 10 for x in range(1,11)]
+            time.sleep(random.choice(list_of_seconds))
+        else:
+            time.sleep(time_to_sleep)
+    
+    def login(self, username: str, password: str):
+        self.username = username
+        self.password = password
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+    def __str__(self):
+        return f'{self.driver.title}\n{self.driver.current_url}'
+    
+    def close(self):
+        self.driver.close()
+
 class ParserBot(TemplateBot):
     def parse(self, url: str) -> float:
         self.driver.get(url)
